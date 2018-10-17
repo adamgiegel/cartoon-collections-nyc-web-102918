8462ff2e71c43c308ff718763f9d94db915d84d8
Adam Giegel <adam.giegel@gmail.com>
2018-10-17 21:59:21 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,8 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.collect do { |name| name.capitalize + "!" }
+  array.collect do |name| 
+    name.capitalize + "!" 
   end
 end
 
