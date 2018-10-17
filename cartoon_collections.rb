2a3db04599857553edba8aab4f28dfe51a0bebf3
Adam Giegel <adam.giegel@gmail.com>
2018-10-17 22:03:54 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,10 +14,10 @@ def long_planeteer_calls(array)
   array.each do |name|
     if name.length >= 4
       return true
-    end
     else
       return false
     end
+  end
 end
 
 def find_the_cheese# code an argument here
